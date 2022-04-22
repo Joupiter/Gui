abf8e965ff9639ff8880a9c4fb766d6e66cb9e76
Joupiter <51340164+Joupiter@users.noreply.github.com>
2022-04-22 18:31:52 +0200

    Update ExamplePaginatedGui.java

--- src/main/java/fr/joupi/gui/example/ExamplePaginatedGui.java
+++ src/main/java/fr/joupi/gui/example/ExamplePaginatedGui.java
@@ -1,38 +1,49 @@
 package fr.joupi.gui.example;
 
+import com.google.common.collect.Lists;
 import fr.joupi.gui.GuiButton;
+import fr.joupi.gui.ItemBuilder;
 import fr.joupi.gui.PageableGui;
+import lombok.Getter;
+import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Arrays;
+import java.util.List;
 
-public class ExamplePaginatedGui extends PageableGui<ExamplePlugin, Material> {
+public class ExamplePaginatedGui extends PageableGui<ExamplePlugin, ItemStack> {
+
+    @Getter private final List<ItemStack> list;
 
     protected ExamplePaginatedGui(ExamplePlugin plugin) {
-        super(plugin, "&6&lMenu avec pages", 3, 9);
+        super(plugin, "&eMenu", 3, 9);
+        this.list = Lists.newLinkedList();
 
-        Arrays.stream(Material.values()).forEach(this::addMaterial);
+        for (int i = 0; i < 64; i++)
+            list.add(new ItemBuilder(Material.ENDER_PEARL).setAmount(i).build());
+
+        list.forEach(this::addItem);
     }
 
     @Override
     public void setup() {
+        setHorizontalLine(18, 27, new ItemBuilder(Material.STAINED_GLASS_PANE).setDyeColor(DyeColor.BLUE).build());
+
         for (int i = 0; i < getPage().countElements(); i++)
-            setItem(i, new GuiButton(new ItemStack(getPage().getElements().get(i))));
+            setItem(i, new GuiButton(getPage().getElements().get(i)));
 
         setItem(21, previousPageButton());
 
         setItem(23, nextPageButton());
-
     }
 
-    private void addMaterial(Material material) {
-        getPagination().addElement(material);
+    private void addItem(ItemStack itemStack) {
+        getPagination().addElement(itemStack);
     }
 
     @Override
     public GuiButton nextPageButton() {
-        return new GuiButton(new ItemStack(Material.ARROW), event -> {
+        return new GuiButton(new ItemBuilder(Material.ARROW).setName("&aSuivant").build(), event -> {
             if (!getPagination().hasNext(getPage())) return;
 
             updatePage(getPagination().getNext(getPage()));
@@ -41,10 +52,11 @@ public class ExamplePaginatedGui extends PageableGui<ExamplePlugin, Material> {
 
     @Override
     public GuiButton previousPageButton() {
-        return new GuiButton(new ItemStack(Material.ARROW), event -> {
+        return new GuiButton(new ItemBuilder(Material.ARROW).setName("&cRetour").build(), event -> {
             if (!getPagination().hasPrevious(getPage())) return;
 
             updatePage(getPagination().getPrevious(getPage()));
         });
     }
+
 }
